4096742912aa839edc8cd583ba6e94fee7acf5fb
Von Ivan Villaruel <vonbv_25@yahoo.com.ph>
2014-06-13 17:53:45 +0800

    1st commit

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -1,6 +1,15 @@
 
+import com.google.common.collect.Lists;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
+import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by von on 6/12/14.
@@ -9,10 +18,30 @@ import java.io.IOException;
 
 public class Indexer {
 
+    static int VECTOR_WIDTH = 4000;
 
     public static void main(String[] args) throws IOException {
 
+        FeatureVectorEncoder encoder = new AdaptiveWordValueEncoder("message");
 
+        encoder.setProbes(2);
+//        BufferedReader reader = new BufferedReader(new FileReader(""));
+
+        String pathname = new File(Indexer.class.getClassLoader().
+                getResource("SMSSpamCollection").getFile()).getAbsolutePath();
+//        System.out.print(pathname);
+        Vector v = new RandomAccessSparseVector(VECTOR_WIDTH);
+
+        BufferedReader reader = new BufferedReader(new FileReader(pathname));
+
+        List<String> doc = Lists.newArrayList();
+
+        //Add documents to storage.
+        List<String>targets = Lists.newArrayList();
+        String line;
+        while ((line=reader.readLine())!= null) {
+            doc.add(line);
+        }
 
     }
 
