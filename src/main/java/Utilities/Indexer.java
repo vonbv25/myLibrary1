c9ce42ed30e03b5223a59c02b1ed2918eee8e1db
vonbv25 <vonbv_25@yahoo.com.ph>
2014-06-14 23:11:31 +0800

    vectorizing documents are in the sample class

--- src/main/java/Utilities/Indexer.java
+++ src/main/java/Utilities/Indexer.java
@@ -5,11 +5,18 @@ import com.google.common.collect.Lists;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
+import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
 import java.util.List;
 
@@ -18,28 +25,49 @@ import java.util.List;
  */
 public class Indexer {
 
-    public static Lists stringLists(StringReader reader) throws IOException{
+public static int VECTOR_WIDTH = 10000;
+
+    public static List tokenized(Reader reader) throws IOException{
 
         Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_46);
-
         TokenStream ts = analyzer.tokenStream("content", reader);
-
         ts = new LowerCaseFilter(Version.LUCENE_46,ts);
-
+        ts = new PorterStemFilter(ts);
         CharTermAttribute charTermAttribute = ts.addAttribute(CharTermAttribute.class);
-
-        List<String> word = Lists.newArrayList();
+        List<String> words = Lists.newArrayList();
 
         while (ts.incrementToken()) {
             char[] termbuffer = charTermAttribute.buffer();
             int termLength = charTermAttribute.length();
-            String w = new String(termbuffer,0,termLength);
-            word.add(w);
+            String w = new String(termbuffer,1,termLength);
+            words.add(w);
+        }
+        return words;
+     }
+
+
+    public static Vector encoder(List<String> tokens, String label) {
+        FeatureVectorEncoder encoder = new AdaptiveWordValueEncoder("content");
+        encoder.setProbes(2);
+
+        Vector v = new RandomAccessSparseVector(VECTOR_WIDTH);
+
+        for (String word : tokens) {
+            encoder.addToVector(word,1,v);
         }
 
+        return new NamedVector(v,label);
 
 
 
     }
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -1,22 +1,60 @@
-import org.apache.mahout.math.DenseVector;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.RandomAccessSparseVector;
 import org.apache.mahout.math.Vector;
-import org.apache.mahout.math.VectorView;
+import org.apache.mahout.math.SequentialAccessSparseVector;
+import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
+import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
+import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by von on 6/10/14.
  */
 public class sample {
 
+    public static Vector encode_text(String document) throws IOException{
+        FeatureVectorEncoder encoder= new AdaptiveWordValueEncoder("text");
+        encoder.setProbes(2);
+        Analyzer analyzer= new StandardAnalyzer(Version.LUCENE_46);
+        StringReader in = new StringReader(document);
+        TokenStream ts= analyzer.tokenStream("content",in);
+        CharTermAttribute termAtt = ts.addAttribute(CharTermAttribute.class);
+        ts.reset();
+        Vector v1= new RandomAccessSparseVector(100);
+        while(ts.incrementToken()){
+            char[] termBuff= termAtt.buffer();
+            int termLen=termAtt.length();
+            String w = new String(termBuff,0,termLen);
+            encoder.addToVector(w,1,v1);
+        }
+        ts.end();
+        ts.close();
+        return new NamedVector(v1, "ham");
 
-    public static void main(String[] args) {
-    double[] x = new double[] {1,2,3,4,5};
+    }
 
+    public static void main(String[] args) throws IOException{
 
-       Vector X = new DenseVector(x);
+        BufferedReader reader = new BufferedReader(new FileReader(
+                sample.class.getClassLoader().getResource("SMSSpamCollection").getFile()
+        ));
+        List<Vector> data = new ArrayList<Vector>();
+        String line;
+        while ((line = reader.readLine())!= null) {
+            data.add(encode_text(line));
+        }
+        System.out.printf("%s\n", new SequentialAccessSparseVector(data.get(90)));
+        System.out.print(data.size());
 
-        for (Vector.Element _x: X.all())
-            System.out.println(_x);
-
-        System.out.print(X.get(0));
 
     }
 
